70b6f625525634fb6d8fa6263b923a1c466cb3ac
jxhnkndl <jkroyston@gmail.com>
2021-03-25 17:56:53 -0400

    Reconfigure server to only start after a successful database connection has been established

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const mongoose = require('mongoose');
 // Config PORT
 const PORT = process.env.PORT || 3000;
 
+// Config local db path
+const LOCALDB_URI = 'mongodb://localhost/workout-test';
+
 // Init Express
 const app = express();
 
@@ -19,12 +22,12 @@ app.use(express.json());
 // Statically serve public directory to client
 app.use(express.static('public'));
 
-// Connect to database (.env variable OR localhost db)
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workoutdb', {
-  useNewUrlParser: true,
-});
-
-// Start listening
-app.listen(PORT, () => {
-  console.log(`App running at http://localhost:${PORT}`);
-});
+// Connect to database and start server if connection is established
+mongoose
+  .connect(process.env.MONGODB_URI || LOCALDB_URI, { useNewUrlParser: true })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`App running at http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => console.log(`Cannot connect to database:\n ${err}`));
