5d3cc27cd5fd248d8e21e530e32211e6e4f41c44
jxhnkndl <jkroyston@gmail.com>
2021-03-28 16:23:56 -0400

    Add index.js script to exercise.html to ensure that exercises can be added to a new workout once the API responds with exercise.html

--- public/exercise.js
+++ public/exercise.js
@@ -13,7 +13,7 @@ const distanceInput = document.querySelector("#distance");
 const completeButton = document.querySelector("button.complete");
 const addButton = document.querySelector("button.add-another");
 const toast = document.querySelector("#toast");
-const newWorkout = document.querySelector(".new-workout")
+const newWorkout = document.querySelector(".new-workout");
 
 let workoutType = null;
 let shouldNavigateAway = false;
@@ -24,7 +24,6 @@ async function initExercise() {
 
   if (location.search.split("=")[1] === undefined) {
     workout = await API.createWorkout()
-    console.log(workout)
   }
   if (workout) {
     location.search = "?id=" + workout._id;
