c2772e79d9df10c0180b34f46506143c639aa7e9
jxhnkndl <jkroyston@gmail.com>
2021-03-28 15:36:18 -0400

    Add PUT route for pushing new exercises into most recent workout

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,28 +1,34 @@
-// Import models
+// Import mongoose and models
+const mongoose = require('mongoose');
 const db = require('../models');
 
 // Create and export API router
 module.exports = (app) => {
-
-  // GET: Get workouts
+  // GET: Get all workouts
   app.get('/api/workouts', async (req, res) => {
     try {
-      const data = await db.Workout.find({});
-      res.json(data);
+      const workouts = await db.Workout.find({});
+      res.json(workouts);
     } catch {
-      res.status(500).json({ success: false });
+      res.status(500).send('Error getting resource: ' + err);
     }
   });
 
-  // POST: Create new workout
-  app.post('/api/workouts', async (req, res) => {
-    console.log(req.body);
+  // PUT: Add exercises to workout
+  app.put('/api/workouts/:id', async (req, res) => {
+    const id = req.params.id;
+    const exercise = req.body;
 
     try {
-      const data = await db.Workout.insert(req.body);
-      console.log(data);
+      const workout = await db.Workout.findOneAndUpdate(
+        { _id: id },
+        { $push: { exercises: exercise } },
+        { new: true }
+      );
+
+      res.json(workout);
     } catch {
-      res.status(500).json({ success: false })
+      res.status(500).send('Error updating resource: ' + err);
     }
   });
 };
